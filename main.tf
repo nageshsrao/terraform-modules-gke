db625f2147a3af2dbc00b51ed2d5c2808be9bd84
nageshsrao <nageshsrao@gmail.com>
2021-01-08 17:21:07 +0000

    2nd commit

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@ module "gke" {
   source  = "tfe.nrao1.2741611427.workshops.arctiq.ca/gxscorp/gke/google"
   version = "1.0.0"
   # insert required variables here
-}
-  # insert required variables here
+
   cluster_name = var.cluster_name
   project_id=var.gcp_project_id
 
